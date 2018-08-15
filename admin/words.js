2a82e572ca5c858c6beb2a5db719b51cbc3ef9b3
Christian <chvorholt@gmail.com>
2018-08-15 21:22:32 +0200

    added missing languages

--- admin/words.js
+++ admin/words.js
@@ -4,96 +4,145 @@
 systemDictionary = {
   "local server settings": {
     "en": "local server settings",
-    "de": "Lokale Server Einstellungen",
-    "ru": "local server settings",
-    "pt": "local server settings",
-    "nl": "local server settings",
-    "fr": "local server settings",
-    "it": "local server settings",
-    "es": "local server settings",
-    "pl": "local server settings"
+    "de": "lokale Servereinstellungen",
+    "ru": "настройки локального сервера",
+    "pt": "configurações do servidor local",
+    "nl": "lokale serverinstellingen",
+    "fr": "paramètres du serveur local",
+    "it": "impostazioni del server locale",
+    "es": "configuración del servidor local",
+    "pl": "ustawienia lokalnego serwera"
   },
   "local_server_ip": {
-    "en": "IP address",
-    "de": "IP-Adresse",
-    "ru": "IP address",
-    "pt": "IP address",
-    "nl": "IP address",
-    "fr": "IP address",
-    "it": "IP address",
-    "es": "IP address",
-    "pl": "IP address"
+    "en": "local server ip",
+    "de": "lokale Server-IP",
+    "ru": "локальный сервер ip",
+    "pt": "ip do servidor local",
+    "nl": "lokale server ip",
+    "fr": "ip serveur local",
+    "it": "IP del server locale",
+    "es": "servidor local ip",
+    "pl": "lokalny serwer ip"
   },
   "local_server_port": {
-    "en": "port",
-    "de": "Port",
-    "ru": "port",
-    "pt": "port",
-    "nl": "port",
-    "fr": "port",
-    "it": "port",
-    "es": "port",
-    "pl": "port"
+    "en": "local server port",
+    "de": "lokaler Server-Port",
+    "ru": "порт локального сервера",
+    "pt": "porta do servidor local",
+    "nl": "lokale serverpoort",
+    "fr": "port du serveur local",
+    "it": "porta del server locale",
+    "es": "puerto del servidor local",
+    "pl": "lokalny port serwera"
+  },
+  "main settings": {
+    "en": "main settings",
+    "de": "Haupteinstellungen",
+    "ru": "Основные настройки",
+    "pt": "configurações principais",
+    "nl": "belangrijkste instellingen",
+    "fr": "Réglages principaux",
+    "it": "impostazioni principali",
+    "es": "ajustes principales",
+    "pl": "Ustawienia główne"
   },
   "off": {
     "en": "off",
     "de": "aus",
-    "ru": "off",
-    "pt": "off",
-    "nl": "off",
-    "fr": "off",
-    "it": "off",
-    "es": "off",
-    "pl": "off"
+    "ru": "от",
+    "pt": "fora",
+    "nl": "uit",
+    "fr": "de",
+    "it": "via",
+    "es": "apagado",
+    "pl": "poza"
   },
-  "on": {"en": "on", "de": "an", "ru": "on", "pt": "on", "nl": "on", "fr": "on", "it": "on", "es": "on", "pl": "on"},
+  "on": {
+    "en": "on",
+    "de": "auf",
+    "ru": "на",
+    "pt": "em",
+    "nl": "op",
+    "fr": "sur",
+    "it": "sopra",
+    "es": "en",
+    "pl": "na"
+  },
   "on save adapter restarts with new config immediately": {
     "en": "on save adapter restarts with new config immediately",
-    "de": "Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet.",
-    "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями",
-    "pt": "on save adapter restarts with new config immediately",
-    "nl": "on save adapter restarts with new config immediately",
-    "fr": "on save adapter restarts with new config immediately",
-    "it": "on save adapter restarts with new config immediately",
-    "es": "on save adapter restarts with new config immediately",
-    "pl": "on save adapter restarts with new config immediately"
+    "de": "Beim Speichern wird der Adapter sofort mit der neuen Konfiguration neu gestartet",
+    "ru": "на перезаписываемом адаптере перезагружается с новой конфигурацией немедленно",
+    "pt": "on save adapter reinicia com nova configuração imediatamente",
+    "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config",
+    "fr": "sur la sauvegarde de l'adaptateur redémarre immédiatement avec la nouvelle configuration",
+    "it": "su save adapter si riavvia immediatamente con la nuova configurazione",
+    "es": "en el adaptador de guardar se reinicia con nueva configuración de inmediato",
+    "pl": "na karcie save natychmiast uruchamia się z nową konfiguracją"
+  },
+  "wiffi IP address": {
+    "en": "Wiffi IP address",
+    "de": "Wiffi IP-Adresse",
+    "ru": "IP-адрес Wiffi",
+    "pt": "endereço IP Wiffi",
+    "nl": "Wiffi IP-adres",
+    "fr": "adresse IP Wiffi",
+    "it": "indirizzo IP Wiffi",
+    "es": "Wiffi dirección IP",
+    "pl": "Wiffi adres IP"
   },
   "wiffi devices": {
-    "en": "Wiffis",
-    "de": "Wiffis",
-    "ru": "Wiffis",
-    "pt": "Wiffis",
-    "nl": "Wiffis",
-    "fr": "Wiffis",
-    "it": "Wiffis",
-    "es": "Wiffis",
-    "pl": "Wiffis"
+    "en": "Wiffi devices",
+    "de": "Wiffi Geräte",
+    "ru": "устройства Wiffi",
+    "pt": "dispositivos Wiffi",
+    "nl": "Wiffi-apparaten",
+    "fr": "appareils Wiffi",
+    "it": "dispositivi Wiffi",
+    "es": "dispositivos Wiffi",
+    "pl": "urządzenia Wiffi"
+  },
+  "wiffi name": {
+    "en": "Wiffi name",
+    "de": "Wiffi Name",
+    "ru": "имя Wiffi",
+    "pt": "nome Wiffi",
+    "nl": "Wiffi naam",
+    "fr": "nom Wiffi",
+    "it": "nome Wiffi",
+    "es": "nombre Wiffi",
+    "pl": "nazwa Wiffi"
+  },
+  "wiffi room": {
+    "en": "Wiffi room",
+    "de": "Wiffi Zimmer",
+    "ru": "Wiffi room",
+    "pt": "quarto Wiffi",
+    "nl": "Wiffi kamer",
+    "fr": "salle Wiffi",
+    "it": "stanza Wiffi",
+    "es": "habitación Wiffi",
+    "pl": "pokój Wiffi"
   },
   "wiffi-wz settings": {
     "en": "Wiffi-wz settings",
     "de": "Wiffi-wz Einstellungen",
-    "ru": "Пример",
-    "pt": "Wiffi-wz settings",
-    "nl": "Wiffi-wz settings",
-    "fr": "Wiffi-wz settings",
-    "it": "Wiffi-wz settings",
-    "es": "Wiffi-wz settings",
-    "pl": "Wiffi-wz settings"
+    "ru": "Настройки Wiffi-wz",
+    "pt": "configurações Wiffi-wz",
+    "nl": "Wiffi-wz instellingen",
+    "fr": "Paramètres Wiffi-wz",
+    "it": "impostazioni Wiffi-wz",
+    "es": "ajustes Wiffi-wz",
+    "pl": "ustawienia Wiffi-wz"
   },
-  "main settings": {
-    "en": "Main settings",
-    "de": "Hauptinstellungen"
-  },
-  "wiffi name": {
-    "en": "Description",
-    "de": "Bezeichnung"
-  },
-  "wiffi IP address": {
-    "en": "IP address",
-    "de": "IP-Adresse"
-  },
-  "wiffi room": {
-    "en": "Room",
-    "de": "Raum"
+  "wiffi type": {
+    "en": "wiffi type",
+    "de": "Wiffi-Typ",
+    "ru": "тип wifi",
+    "pt": "tipo de wifi",
+    "nl": "wifi type",
+    "fr": "type de wifi",
+    "it": "tipo di wifi",
+    "es": "tipo wifi",
+    "pl": "typ wifi"
   }
 };
